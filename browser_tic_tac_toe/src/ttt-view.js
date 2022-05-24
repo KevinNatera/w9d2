0101f5dd315085e3dd868e4c7c560ac75d91a04f
Kevin <NateraKevin1@gmail.com>
2022-05-24 18:33:38 -0400

    setupBoard

--- browser_tic_tac_toe/src/ttt-view.js
+++ browser_tic_tac_toe/src/ttt-view.js
@@ -1,8 +1,20 @@
 class View {
   constructor(game, el) {}
 
-  setupBoard() {}
-  
+  setupBoard() {
+    let ul = document.createElement('ul')
+    ul.width = 500
+    ul.display = "flex"
+    ul.flexWrap = "wrap"
+
+    for(let i = 0; i < 10; i++) {
+      let li = document.createElement('li');
+      li.dataset.pos = i
+      ul.appendChild(li);
+    }
+
+  }
+    
   bindEvents() {}
 
   handleClick(e) {}
