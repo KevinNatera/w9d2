d119704ae3b5a867108701f9558d9175855f29fd
Kevin <NateraKevin1@gmail.com>
2022-05-24 18:19:30 -0400

    revision

--- browser_tic_tac_toe/src/index.js
+++ browser_tic_tac_toe/src/index.js
@@ -2,5 +2,5 @@ const View =  require("./ttt-view");
 const Game = require("../ttt_node/game");
 
 document.addEventListener("DOMContentLoaded", () => {
-    figure = document.querySelector('.ttt')
+      document.querySelector('.ttt')
 });
